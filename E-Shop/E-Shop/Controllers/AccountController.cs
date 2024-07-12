35e1cbf7a3da06c1f7746b37541788e1287d6b46
Ertugrul <57365047+officialerto@users.noreply.github.com>
2024-07-12 22:19:22 +0300

    ECommerceProject_.NET
    Projeyi burada bitiriyorum .NET Core projesine geçeceğim. Yeteri kadar hatırladığımı düşünüyorum

---
+++ E-Shop/E-Shop/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using DataAccessLayer.Context;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace E_Shop.Controllers
+{
+    public class AccountController : Controller
+    {
+        DataContext db = new DataContext();
+
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User data)
+        {
+            var bilgiler = db.Users.FirstOrDefault(x => x.Email == data.Email && x.Password == data.Password);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.Email, false);
+                Session["Mail"] = bilgiler.Email.ToString();
+                Session["Ad"] = bilgiler.Name.ToString();
+                Session["Soyad"] = bilgiler.SurName.ToString();
+                Session["userid"] = bilgiler.Id.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.hata = "Mail veya şifre hatalı!";
+
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Register(User data)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(data);
+                data.Role = "User";
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Hatalı");
+            return View("Login", data);
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            //return RedirectToActionPermanent("Index");
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ E-Shop/ecommercesql2.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO Users (Name, SurName, Email, UserName, Password, RePassword, Role) 
+values ('Gökay', 'Yılmaz', 'gokay@mail.com', 'gokaylmz', '1234', '1234', 'Admin')
